9eb11308b8da6551e395332a6af1511b6e086bab
Jing CHC <jing.chc@gmail.com>
2016-05-30 17:07:46 +0800

    0530 recap

--- Mr-Ride-iOS/SideBarTableViewCell.swift
+++ Mr-Ride-iOS/SideBarTableViewCell.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class SideBarTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var dot: UIView!
+    @IBOutlet weak var pageName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        set()
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        
+//        self.dot.backgroundColor = UIColor.mrWhiteColor()
+        self.layer.backgroundColor = UIColor.mrDarkSlateBlueColor().CGColor
+        self.pageName.textColor = UIColor.mrWhite50Color()
+        
 
         // Configure the view for the selected state
+    }
+    
+    func set() {
+        
+        self.layer.backgroundColor = UIColor.mrDarkSlateBlueColor().CGColor
+        
+        self.dot.layer.cornerRadius = self.dot.frame.size.width / 2
+        self.dot.backgroundColor = UIColor.mrDarkSlateBlueColor()
+        self.dot.layer.shadowColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.25).CGColor
+        self.pageName.font = UIFont.mrTextStyle7Font()
+        self.pageName.textColor = UIColor.mrWhite50Color()
+        self.pageName.shadowColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.25)
     }
 
 }
