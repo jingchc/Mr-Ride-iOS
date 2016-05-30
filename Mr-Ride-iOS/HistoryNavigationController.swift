41974bce5f3a04fa206e12aaf266d1ee320bb09a
Jing CHC <jing.chc@gmail.com>
2016-05-30 16:05:00 +0800

    sidebar - slide - done

---
+++ Mr-Ride-iOS/HistoryNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  HistoryNavigationController.swift
+//  Mr-Ride-iOS
+//
+//  Created by 莊晶涵 on 2016/5/30.
+//  Copyright © 2016年 AppWorks School Jing. All rights reserved.
+//
+
+import UIKit
+
+class HistoryNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Mr-Ride-iOS/HomeViewController.swift
+++ Mr-Ride-iOS/HomeViewController.swift
@@ -13,11 +13,29 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         set()
-
+        
     }
     
     func set() {
         self.view.backgroundColor = UIColor.mrLightblueColor()
+        
+        let templateTittleIcon = UIImage(named: "icon-bike")?.imageWithRenderingMode(.AlwaysTemplate)
+        let tittleImageView = UIImageView(image: templateTittleIcon)
+        tittleImageView.tintColor = UIColor.mrWhiteColor()
+        self.navigationItem.titleView = tittleImageView
+        
+        let templateMenuIcon = UIImage(named: "icon-menu")?.imageWithRenderingMode(.AlwaysTemplate)
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.mrWhiteColor()
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: templateMenuIcon, style: .Plain, target: nil, action: nil)
+       
+        if self.revealViewController() != nil {
+            self.navigationItem.leftBarButtonItem?.target = self.revealViewController()
+            self.navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+        
+        
     }
 
 }
